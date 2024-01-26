57dac2116174d825e0d4b801fbc99f052cc86ac6
programming101dev <113087924+programming101dev@users.noreply.github.com>
2024-01-25 23:00:57 -0800

    update tp latest build process

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ ! -f "./CMakeLists.txt" ]; then
+  echo "You must run ./generate-cmakelists.sh first"
+  exit 1
+fi
+
 if [ ! -d "./build/" ]; then
   echo "You must run ./change-compiler.sh first"
   exit 1
--- change-compiler.sh
+++ change-compiler.sh
@@ -7,7 +7,7 @@ cppcheck_name="cppcheck"
 usage()
 {
     echo "Usage: $0 -c <c++ compiler> [-f <clang-format>] [-t <clang-tidy>] [-k <cppcheck>]"
-    echo "  -c c++ compiler   Specify the c++ compiler name (e.g. g++ or clang++)"
+    echo "  -c c++ compiler   Specify the c++ compiler name (e.g. gcc or clang)"
     echo "  -f clang-format   Specify the clang-format name (e.g. clang-tidy or clang-tidy-17)"
     echo "  -t clang-tidy     Specify the clang-tidy name (e.g. clang-tidy or clang-tidy-17)"
     echo "  -k cppcheck       Specify the cppcheck name (e.g. cppcheck)"
@@ -50,4 +50,4 @@ if [ ! -d "./.flags" ]; then
     ./generate-flags.sh
 fi
 
-cmake -S . -B build -DCMAKE_CXX_COMPILER="$cxx_compiler" -DCLANG_FORMAT_NAME="$clang_format_name" -DCLANG_TIDY_NAME="$clang_tidy_name" -DCPPCHECK_NAME="$cppcheck_name" -DCMAKE_BUILD_TYPE=Debug
+cmake -S . -B build -DCMAKE_C_COMPILER="$cxx_compiler" -DCLANG_FORMAT_NAME="$clang_format_name" -DCLANG_TIDY_NAME="$clang_tidy_name" -DCPPCHECK_NAME="$cppcheck_name" -DCMAKE_BUILD_TYPE=Debug
