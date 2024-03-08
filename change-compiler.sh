b6a391daa07173e73201370d53e995fca270f2d6
programming101dev <113087924+programming101dev@users.noreply.github.com>
2024-03-07 22:42:21 -0800

    fix variable name

--- change-compiler.sh
+++ change-compiler.sh
@@ -4,7 +4,7 @@ cxx_compiler=""
 clang_format_name="clang-format"
 clang_tidy_name="clang-tidy"
 cppcheck_name="cppcheck"
-sanitizers="address,leak,pointer-overflow,undefined"
+sanitizers="address,leak,pointer_overflow,undefined"
 
 # Function to display script usage
 usage()
