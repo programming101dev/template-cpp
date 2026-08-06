54ca10245734df9af77e7938356134776df75ac3
programming101dev <113087924+programming101dev@users.noreply.github.com>
2026-08-06 03:18:09 -0700

    Enforce semantic source contracts

--- test/test_display.cpp
+++ test/test_display.cpp
@@ -6,7 +6,13 @@ extern "C"
 #include <cstdio>
 #include <cstring>
 #include <p101_c/p101_stdio.h>
-#include <p101_io/io.h>
+#include <p101_io/p101_aio.h>
+#include <p101_io/p101_fcntl.h>
+#include <p101_io/p101_poll.h>
+#include <p101_io/p101_stdio.h>
+#include <p101_io/p101_unistd.h>
+#include <p101_io/sys/p101_select.h>
+#include <p101_io/sys/p101_uio.h>
 
 static p101_error *error;
 static p101_env   *env;
